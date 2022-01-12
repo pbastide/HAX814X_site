35081e3f5cd48db192162f8b9a9963ba4d94a626
Paul Bastide <paul.bastide@gmail.com>
2022-01-12 10:54:40 +0100

    Change name to HAX814X

--- R/build2.R
+++ R/build2.R
@@ -1,2 +1,10 @@
 # An optional custom script to run after Hugo builds your site.
 # You can delete it if you do not need it.
+# # clean up tex files
+# latex_files <- list.files('static', pattern = ".*TD_.*.pdf",
+#                           recursive = TRUE, full.names = TRUE)
+# file.remove(latex_files)
+# # clean up html files
+# html_files <- list.files('static', pattern = "*.html",
+#                           recursive = TRUE, full.names = TRUE)
+# file.remove(html_files)
